8dc001420076d72a05712eefba1a15f95e7f8c5c
nkosi-tauro <nkosilati23@yahoo.com>
2020-05-24 23:18:37 +0200

    updated prisma and added new mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){
+        //TODO add authentication
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+  
+};
 
 module.exports = Mutations;
